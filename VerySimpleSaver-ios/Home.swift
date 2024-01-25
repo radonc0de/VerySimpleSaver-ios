033946898e91111aabae400a1130815387745a85
Keith L <keithlehman@proton.me>
2024-01-24 19:45:25 -0500

    Initial commit

---
+++ VerySimpleSaver-ios/Home.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  VerySimpleSaver-ios
+//
+//  Created by Keith L on 1/24/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+	var body: some View {
+		ProgressView(value: 10, total: 15)
+	}
+}
+
+#Preview {
+    Home()
+}
---
+++ VerySimpleSaver-ios/VerySimpleSaver_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerySimpleSaver_iosApp.swift
+//  VerySimpleSaver-ios
+//
+//  Created by Keith L on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VerySimpleSaver_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
